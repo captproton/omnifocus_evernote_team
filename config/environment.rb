41598b0072ac906c1e68cb13df0c51fa7f30f00d
captproton <carl@wdwhub.net>
2021-08-28 17:39:22 -0700

    CHANGE: moved requirements and settings into config

---
+++ config/environment.rb
@@ -0,0 +1,14 @@
+# gems and stdlib
+require 'fileutils'
+require 'nokogiri'
+require 'rubygems'
+require 'thor'
+require 'rails/generators/actions'
+require 'require_all'
+
+# files & directories
+require_all 'lib'
+require_all 'models'
+require_all 'controllers'
+
+
